b122ab90bc196aed3090883a95a357e09218190f
liuyun <echochen541@163.com>
2016-03-15 13:02:36 +0800

    add testcase5 which contains 4800 edges

--- future_net/src/com/routesearch/route/Route.java
+++ future_net/src/com/routesearch/route/Route.java
@@ -120,8 +120,8 @@ public final class Route {
 					if (cost < minCost) {
 						minCost = cost;
 						minPath = new ArrayList<Integer>(path);
-						// System.out.println("minPath is " + minPath);
-						// System.out.println("minCost is " + minCost);
+						System.out.println("minPath is " + minPath);
+						System.out.println("minCost is " + minCost);
 						// optimize path
 					}
 				}
