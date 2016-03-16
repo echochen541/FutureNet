417ac2901857b655a2ba4a2900a6e5bdc5f6bfd7
youheiwoke <slimyoung1908@gmail.com>
2016-03-16 11:17:23 +0800

    optimizePath() method added.

--- future_net/src/com/routesearch/route/Route.java
+++ future_net/src/com/routesearch/route/Route.java
@@ -142,4 +142,149 @@ public final class Route {
 			}
 		}
 	}
+
+	private static void optimizePath(List<Integer> path) {
+		int pre = -1, post = -1, preIndex = -1, weight = 0;
+		List<List<Integer>> optimizedSubPaths = new
+
+		LinkedList<List<Integer>>();
+		List<Integer> prevs = new ArrayList<Integer>();
+		List<Integer> posts = new ArrayList<Integer>();
+
+		// Find the opportunity of optimization in the minPath.
+		// pre and post is the start and end vertex of a optimization
+		// opportunity, respectively.
+		int i = 0;
+		int presentV = -1, prePresentV = -1;
+		for (Iterator<Integer> it = path.iterator();
+
+		it.hasNext();) {
+			presentV = it.next();
+			if (pre == -1 && includingSet.contains(presentV)) {
+				// determine the start vertex
+				pre = presentV;
+				preIndex = i;
+			} else if (pre != -1 && includingSet.contains(presentV) && preIndex == (i - 1)) {
+				// change the start point to thisvertex
+				pre = presentV;
+				preIndex = i;
+			} else if (pre != -1 && includingSet.contains(presentV) && preIndex != (i - 1)) {
+				// determine the end vertex
+				post = presentV;
+				weight += edgeWeights[prePresentV][presentV];
+			} else if (pre != -1) {
+				weight += edgeWeights[prePresentV][presentV];
+			}
+
+			if (pre != -1 && post != -1) {
+				// Optimize the sub-path
+				List<Integer> sub = optimizeSubPath(pre, post, weight);
+				if (sub != null) {
+					optimizedSubPaths.add(sub);
+					prevs.add(pre);
+					posts.add(post);
+				}
+
+				pre = post;
+				preIndex = i;
+				post = -1;
+				weight = 0;
+			}
+
+			prePresentV = presentV;
+			i++;
+		}
+
+		// Substitute with the optimized sub path.
+		int size = optimizedSubPaths.size();
+		for (int j = 0; j < size; j++) {
+			List<Integer> subPath = optimizedSubPaths.get(j);
+			int end = posts.get(j);
+			int startIndex = path.indexOf(prevs.get(j)) + 1;
+			int tmp = path.get(startIndex);
+
+			// Remove old sub path.
+			while (tmp != end) {
+				path.remove(startIndex);
+			}
+
+			// Add new sub path.
+			for (Iterator<Integer> it = subPath.iterator(); it.hasNext();) {
+				path.add(startIndex, it.next());
+			}
+		}
+	}
+
+	/**
+	 * Used by optimizePath method. Based on Dijkstra algorithm.
+	 */
+	private static List<Integer> optimizeSubPath(int start, int end, int maxWeight) {
+		int prevVertex[] = new int[600];
+		int distance[] = new int[600];
+		boolean[] isVisited = new boolean[600];
+		List<Integer> aboutToVisit = new LinkedList<Integer>();
+
+		for (int i = 0; i < 600; i++) {
+			distance[i] = Integer.MAX_VALUE;
+		}
+		prevVertex[end] = -1;
+
+		aboutToVisit.add(start);
+		distance[start] = 0;
+		int min = Integer.MAX_VALUE; // Minimum distance at present.
+		int minVertex = start; // Vertex that has the minimum distance at
+								// present.
+		while (!aboutToVisit.isEmpty()) {
+			// Take out the first vertex of the list.
+			aboutToVisit.remove((Object) minVertex);
+			int prev = minVertex;
+			int prevDistance = distance[prev];
+			List<Integer> neighb = neighbors.get(prev);
+
+			// Expand the neighbors to the about to visit list.
+			for (Iterator<Integer> iterator = neighb.iterator(); iterator.hasNext();) {
+				int tmpV = iterator.next();
+				int tmpDis = prevDistance + edgeWeights[prev][tmpV];
+				if (tmpDis >= maxWeight) {
+					continue;
+				}
+				if (tmpV == end) {
+					distance[tmpV] = tmpDis;
+					isVisited[tmpV] = true;
+					prevVertex[tmpV] = prev;
+					continue;
+				}
+
+				distance[tmpV] = tmpDis;
+				prevVertex[tmpV] = prev;
+				if (!isVisited[tmpV]) {
+					isVisited[tmpV] = true;
+					aboutToVisit.add(tmpV);
+				}
+			}
+
+			// Find the vertex in the list that has the minimum distance.
+			min = Integer.MAX_VALUE;
+			for (Iterator<Integer> it =
+
+					aboutToVisit.iterator(); it.hasNext();) {
+				int tmp = it.next();
+				if (distance[tmp] < min) {
+					minVertex = tmp;
+				}
+			}
+		}
+
+		List<Integer> path = new LinkedList<Integer>();
+		int prev = prevVertex[end];
+		if (prev == -1) {
+			return null;
+		}
+		do {
+			path.add(prev);
+			prev = prevVertex[prev];
+		} while (prev != start);
+
+		return path;
+	}
 }
