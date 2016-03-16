d63a812a97d0da414c572a2a2a4bcc103eca5158
Echo Chen <echochen541@163.com>
2016-03-16 22:05:41 +0800

    little change

--- future_net/src/com/filetool/main/Main.java
+++ future_net/src/com/filetool/main/Main.java
@@ -24,11 +24,11 @@ public class Main {
 		// String resultFilePath = args[2];
 
 		String graphFilePath = System.getProperty("user.dir").replaceAll("\\\\", "/")
-				+ "/test/test-case/case50-15/case2/topo.csv";
+				+ "/test/case2/topo.csv";
 		String conditionFilePath = System.getProperty("user.dir").replaceAll("\\\\", "/")
-				+ "/test/test-case/case50-15/case2/demand.csv";
+				+ "/test/case2/demand.csv";
 		String resultFilePath = System.getProperty("user.dir").replaceAll("\\\\", "/")
-				+ "/test/test-case/case50-15/case2/result.csv";
+				+ "/test/case2/result.csv";
 
 		LogUtil.printLog("Begin");
 
